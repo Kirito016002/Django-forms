67d83a9d25561c6141f0edf639362079c945b327
Kirito016002 <016002GenshinImpact001@gmail.com>
2024-03-03 23:19:08 +0500

    'contact'

--- DjangoForms/urls.py
+++ DjangoForms/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from main import views as contact_views
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('main.urls')),
+    path('contacts/', contact_views.contact_view, name='contacts'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from . import models
+from . import forms
 
-# Create your views here.
+
+def index(request):
+    tasks = models.Task.objects.all()
+    return render(request, 'index.html', {'tasks': tasks})
+
+
+def about(request):
+    self = models.About.objects.all()
+    return render(request, 'about.html', {'self': self})
+
+
+def blog(request):
+    notes = models.Note.objects.all()
+    return render(request, 'blog.html', {'notes': notes})
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = forms.ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = forms.ContactForm()
+    context = {'form': form}
+    return render(request, 'contacts.html', context)
